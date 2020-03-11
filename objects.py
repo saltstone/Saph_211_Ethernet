8971ea08e4e70c64191a5f629b808edcc3fc087c
Leo <leopold.clement@laposte.net>
2020-03-11 20:23:35 +0100

    Ca avance doucement

--- objects.py
+++ objects.py
@@ -31,6 +31,9 @@ class Dommaine_colision():
             -{} evenements à venir
             -{} evenement passés
             """.format(self.time, len(self.machines), len(self.next_events ), len(self.past_events))
+    
+    def visiblement_libre(self):
+        return self.etat.visiblement_libre
 
 class Machine():
     """
@@ -45,14 +48,41 @@ class Machine():
         self.loi = loi
         self.etat = M_INACTIF
         self.nombre_echec = 0
+        self.longueur_message = 0
     
     def ajout_event(self, event):
         self.events.append(event)
         self.events.sort()
         self.dommaine.ajout_event(event)
 
-    def attendre(self):
-        dt = self.loi(self.nombre_echec)
+    def attendre(self, dt = None):
+        """
+        Ordonne a la machine d'attendre pendant la duree dt, ou bien jusqu'à une duree donnee par sa loi aleatoire
+
+        Parameters
+        ----------
+        dt : int, optional
+            temps d'attente. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        if dt == None:
+            dt = self.loi(self.nombre_echec)
+        Event_fin_attente(dt, self)
+    
+    def parlant(self):
+        return self.etat.parle
+    
+    def transmetre_message(self, longueur):
+        self.longueur_message = 0
+        if self.dommaine.visiblement_libre() :
+            Event_fin_critique('', self)
+        else :
+            self.attendre(temps_init_emition)
+            
         
         
 class Etat_dommaine():
@@ -70,6 +100,10 @@ class Etat_dommaine():
         """
         if type in ('f', 'r', 'c', 'o'):
             self.type = type
+            if type == 'o':
+                self.visiblement_libre = False
+            else:
+                self.visiblement_libre = True
         else:
             raise ValueError
     def __eq__(self, autre):
@@ -88,6 +122,10 @@ class Etat_machine():
         """
         if type in ('i', 'd', 'e', 'a'):
             self.type = type
+            if type in ['i', 'a']:
+                self.parle = False
+            else:
+                self.parle = True
         else:
             raise ValueError
     def __eq__(self, autre):
@@ -130,12 +168,18 @@ class Loi():
             X = 0
         return int(round(X))
 
-class Event_debut_emition(Event):
-    def __init__(self, a ):
-        pass
+class Event_fin_critique(Event):
+    """
+    Evenement marquant la fin de la période critique
+    """
+    def __init__(self, time, machine):
+        self.time = machine.dommaine.time + temps_init_emition
+        self.machine = machine
+    
 
+class Event_fin_attente(Event):
 
-
+class Event_fin_transmition(Event):
 
 
 
